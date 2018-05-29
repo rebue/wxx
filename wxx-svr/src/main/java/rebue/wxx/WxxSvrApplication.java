f849a7c2714b559c2f0b9767aa2aac28f33205e0
zbz <nnzbz@163.com>
2018-05-30 07:25:53 +0800

    + 微薄利商超的分支

--- wxx-svr/src/main/java/rebue/wxx/WxxSvrApplication.java
+++ wxx-svr/src/main/java/rebue/wxx/WxxSvrApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 排除json的转换器，否则微信要求返回纯文本就会返回json字符串而出错
-@SpringBootApplication(exclude = { rebue.sbs.smx.JacksonConfigurer.class })
+//@SpringBootApplication(exclude = { rebue.sbs.smx.JacksonConfig.class })
+@SpringBootApplication
 @EnableFeignClients(basePackages = { "rebue.wxx.access.token.svr.feign" })
 @EnableDiscoveryClient
 @EnableCircuitBreaker
