23d920c6dd43e740959a14fd17446f9ecaeb5ffb
zbz <nnzbz@qq.com>
2018-11-05 17:12:05 +0800

    * 优化代码

--- wxx-access-token-svr/src/test/java/rebue/wxx/access/token/server/AccessTokenServerApplicationTests.java
+++ wxx-access-token-svr/src/test/java/rebue/wxx/access/token/server/AccessTokenServerApplicationTests.java
@@ -8,11 +8,11 @@ import rebue.wheel.OkhttpUtils;
 
 public class AccessTokenServerApplicationTests {
 
-    private final static String _hostUrl = "http://120.77.220.106/wxx-access-token-svr";
+    private final static String _hostUrl = "http://127.0.0.1:9000/wxx-access-token-svr";
 
     @Test
     public void test01() throws IOException {
-        String url = _hostUrl + "/wx/access/token";
+        final String url = _hostUrl + "/wx/access/token";
         OkhttpUtils.post(url);
     }
 
