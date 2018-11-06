5822948649f5f0af9ccd0e8db7cf82d60f5a992f
zbz <nnzbz@qq.com>
2018-11-06 14:24:42 +0800

    * 修正签名错误的BUG

--- wxx-svr/src/main/java/rebue/wxx/ctrl/WxxResponseRestCtrl.java
+++ wxx-svr/src/main/java/rebue/wxx/ctrl/WxxResponseRestCtrl.java
@@ -101,38 +101,21 @@ public class WxxResponseRestCtrl {
         _log.info("接收到微信授权回调: {}，{}", code, state);
         final Map<String, Object> userInfo = wxxResponseSvc.authorizeCode(code);
         ModelAndView modelAndView;
-//        String modelAndView;
         if (userInfo == null) {
             _log.info("返回获取微信用户信息失败页面");
             modelAndView = new ModelAndView("GetWxUserInfoFail");
-//            modelAndView = "获取微信用户信息失败";
         } else {
+            // 给用户信息map添加state
+            userInfo.put("state", state);
             _log.info("给用户信息map添加签名(重定向登录页面需要签名)");
             SignUtils.sign1(userInfo, wxLoginCallbackSignKey);
-            userInfo.put("state", state);
             _log.info("跳转用户登录页面: {}:{}", wxLoginCallbackMethodType, wxLoginCallbackUrl);
             modelAndView = new ModelAndView("ForwardUserLogin");
             modelAndView.addObject("methodType", wxLoginCallbackMethodType);
             modelAndView.addObject("forwardUrl", wxLoginCallbackUrl);
-//            modelAndView.addObject("forwardUrl", "https://www.baidu.com");
             modelAndView.addObject("userInfo", userInfo);
-
-//            String inputs = "";
-//            for (final Entry<String, Object> item : userInfo.entrySet()) {
-//                inputs += "<input name=\"" + item.getKey() + "\" value=\"" + item.getValue() + "\" type=\"hidden\" />";
-//
-//            }
-//            modelAndView = "<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + "<title>微信用户登录</title>\n" + "<meta charset=\"UTF-8\">\n"
-//                    + "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1,user-scalable=0\">\n"
-//                    + "<link rel=\"stylesheet\" href=\"https://res.wx.qq.com/open/libs/weui/1.1.2/weui.css\" />\n" + "</head>\n" + "\n" + "<body ontouchstart>\n"
-//                    + "    <div id=\"container\">获取微信用户信息成功，正在跳转......</div>\n" + "    <form name=\"form1\" method=\"" //
-//                    + wxLoginCallbackMethodType + "\" action=\"" + wxLoginCallbackUrl + "\">\n" //
-//                    + inputs + "\n" + "    </form>\n" + "</body>\n" + "<script type=\"text/javascript\">\n" + "    document.form1.submit();\n" + "</script>\n" + "</html>";
-
-//            modelAndView = "获取微信用户信息成功";
         }
         return modelAndView;
-//        return new ModelAndView(new RedirectView("https://www.baidu.com"));
     }
 
 }
